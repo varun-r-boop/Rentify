e0a2702a0d917630d23a8f958c8d1782129bab42
varun-r-boop <varunpanthom6555@gmail.com>
2024-09-22 16:08:14 +0530

    Update environment.ts

--- Frontend-Rentify/src/environment/environment.ts
+++ Frontend-Rentify/src/environment/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: false,
-    apiUrl: 'https://d360-test-dotnet.azurewebsites.net',
+    apiUrl: 'https://d360-test-dotnet-hpabg3cteyf8dveh.centralindia-01.azurewebsites.net',
   };
